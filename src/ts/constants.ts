9d00626d455b34c2934d786b893b751c836ea89f
evanildo <evanildo.sf91@gmail.com>
2023-02-25 13:33:03 -0300

    feat: Create file constants

---
+++ src/ts/constants.ts
@@ -0,0 +1,10 @@
+export const appElemets = {
+    pokemonId: (<Element>document.querySelector(".pokemon_id")),
+    pokemonName: (<Element>document.querySelector(".pokemon_name")),
+    pokemonImage: (<HTMLImageElement>document.querySelector(".pokemon_image")),
+    form: (document.querySelector(".form")),
+    input: (<HTMLInputElement>document.querySelector(".input")),
+    buttonPrev: document.querySelector(".btn_prev"),
+    buttonNext: document.querySelector(".btn_next")
+}
+
