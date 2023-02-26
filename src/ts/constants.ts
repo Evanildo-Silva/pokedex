2712a22223d4f02df8e0991bb9926831e3c5970d
evanildo <evanildo.sf91@gmail.com>
2023-02-26 15:42:12 -0300

    feat: Create modal

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,10 +1,12 @@
 export const appElemets = {
-    pokemonId: (<Element>document.querySelector(".pokemon_id")),
-    pokemonName: (<Element>document.querySelector(".pokemon_name")),
-    pokemonImage: (<HTMLImageElement>document.querySelector(".pokemon_image")),
-    form: (document.querySelector(".form")),
-    input: (<HTMLInputElement>document.querySelector(".input")),
-    buttonPrev: document.querySelector(".btn_prev"),
-    buttonNext: document.querySelector(".btn_next")
-}
-
+  pokemonId: <Element>document.querySelector(".pokemon_id"),
+  pokemonName: <Element>document.querySelector(".pokemon_name"),
+  pokemonImage: <HTMLImageElement>document.querySelector(".pokemon_image"),
+  form: document.querySelector(".form"),
+  input: <HTMLInputElement>document.querySelector(".input"),
+  buttonPrev: document.querySelector(".btn_prev"),
+  buttonNext: document.querySelector(".btn_next"),
+  modal: document.querySelector("dialog"),
+  buttonShowModal: document.querySelector(".btn_info"),
+  buttonCloseModal: document.querySelector(".btn_close_modal"),
+};
